faa2e7b472dfd3a37928ff4da5fceeaf95508fda
Ilya Slatov <slatovil@gmail.com>
2026-06-28 19:04:32 +0300

    export/imprort files

--- servo_iv/robot_pca9865.py
+++ servo_iv/robot_pca9865.py
@@ -1,8 +1,7 @@
 from . import Pca
-import pandas as pd
+import csv
 import lgpio
 import time
-import csv
 
 class Robot_pca(Pca):
     """
@@ -13,7 +12,7 @@ class Robot_pca(Pca):
         pin_bus_address: (int, hex) - шина и адрес i2c
         freq - частота подключенных устройств
     """
-    def __init__(self, pin_bus_address: tuple = (1, 0x40), freq: int = 50):
+    def __init__(self, pin_bus_address: tuple = (1, 0x40), freq: int = 50, count_servo: int = 16):
         super().__init__(pin_bus_address, freq)
         self.body: dict[str, int | None | tuple] = {
             'hand_left_0': None,
@@ -33,12 +32,14 @@ class Robot_pca(Pca):
             'leg_right_3' : None,
             'leg_right_4' : None,
             'hand' : tuple(range(6)), 
-            'leg' : tuple(range(6, 16))
+            'leg' : tuple(range(6, count_servo))
         }
-        self.centers = (1500) * 16
+        self.count_servo = count_servo
+        self.centers = (1500) * count_servo
 
     def define_servo(self):
-        for i in range(16):
+        """Определение сервопривода."""
+        for i in range(self.count_servo):
             self.servo_run(i, 1500)
             time.sleep(1)
             self.servo_run(i, 1300)
@@ -53,5 +54,18 @@ class Robot_pca(Pca):
                     self.body[inp] = i
     
     def calibrate(self):
-        df = 
+        """Калибровка серво."""
+        try:
+            with open('calibration.csv', mode='r') as file:
+                self.data = tuple(int(x) for x in list(csv.reader(file))[0])
+            if len(self.data) != 16: raise FileNotFoundError
+            print(self.data)
+
+        except FileNotFoundError:
+            for i in range(16):
+                pass
+            with open('calibration.csv', mode='w', newline='') as file:
+                writer = csv.writer(file)
+                writer.writerow(data)
+                
 
