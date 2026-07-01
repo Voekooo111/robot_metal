876f8ebcfed693ba544b181b4accc540533dab9c
Voekooo111 <79006111+Voekooo111@users.noreply.github.com>
2026-07-01 14:43:25 +0300

    Create robot_pca9685.py

---
+++ servo_iv/robot_pca9685.py
@@ -0,0 +1,82 @@
+from .pca9685 import Pca
+import csv
+import lgpio
+import time
+
+class Robot_pca(Pca):
+    """
+    Робот на PCA9685.
+    Класс подключается по i2c шине к PCA9685. И управляет серво, подключенными к PCA.
+    
+    Args:
+        pin_bus_address: (int, hex) - шина и адрес i2c
+        freq - частота подключенных устройств
+    """
+    def __init__(self, pin_bus_address: tuple = (1, 0x40), freq: int = 50, count_servo: int = 16):
+        super().__init__(pin_bus_address, freq)
+        self.body: dict[str, int | None] = {
+            'hand_left_0': None,
+            'hand_left_1': None,
+            'hand_left_2': None,
+            'hand_right_0': None,
+            'hand_right_1': None,
+            'hand_right_2': None,
+            'leg_left_0' : None,
+            'leg_left_1' : None,
+            'leg_left_2' : None,
+            'leg_left_3' : None,
+            'leg_left_4' : None,
+            'leg_right_0' : None,
+            'leg_right_1' : None,
+            'leg_right_2' : None,
+            'leg_right_3' : None,
+            'leg_right_4' : None,
+        }
+        self.bodypart: dict[str, None | tuple] = {
+            'hand' : tuple(range(6)), 
+            'leg' : tuple(range(6, count_servo))
+        }
+        self.count_servo = count_servo
+        self.centers = [1500] * count_servo
+
+    def define_servo(self):
+        """Определение сервопривода."""
+        for i in range(self.count_servo):
+            self.servo_run(i, 1500)
+            time.sleep(1)
+            self.servo_run(i, 1300)
+            time.sleep(1)
+            self.servo_run(i, 1800)
+            time.sleep(1)
+            flag_input = True
+            while flag_input:
+                inp = input("Какая часть робота? _")
+                if inp in self.body.keys():
+                    flag_input = False
+                    self.body[inp] = i
+    
+    def calibrate(self):
+        """Калибровка серво."""
+        try:
+            with open('calibration.csv', mode='r') as file:
+                self.centers = list(int(x) for x in list(csv.reader(file))[0])
+            if len(self.centers) != self.count_servo: raise FileNotFoundError
+            print(self.centers)
+
+        except FileNotFoundError:
+            for i in range(self.count_servo):
+                self.servo_run(list(self.body.values())[i], 1500)
+                value = input("Середина сервопривода(мс). Для сохранения (-1). __")
+                while value != '-1':
+                    try:
+                        value = int(value)
+                        self.servo_run(list(self.body.values())[i], value)
+                        self.centers[i] = value
+                        value = input("Середина сервопривода(мс). Для сохранения (-1). __")
+                    except ValueError:
+                        print('Введите число')
+
+            with open('calibration.csv', mode='w', newline='') as file:
+                writer = csv.writer(file)
+                writer.writerow(self.centers)
+                
