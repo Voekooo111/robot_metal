b8cdbfaf91a0c32394ce46ded9981bccc3d20208
Ilya Slatov <slatovil@gmail.com>
2026-07-02 13:44:23 +0300

    minimal site without function

--- servo_iv/README.md
+++ servo_iv/README.md
@@ -6,3 +6,35 @@ sudo apt install swig python-dev python3-dev python-setuptools python3-setuptool
 sudo apt install python3-lgpio
 pip install flask
 ```
+
+### Сервер raspberry
+Проверка
+```bash
+sudo systemctl status avahi-daemon
+```
+Если нет, то:
+```bash
+sudo apt update
+sudo apt install avahi-daemon
+sudo systemctl enable avahi-daemon
+sudo systemctl start avahi-daemon
+```
+---
+```bash
+hostname
+```
+http://\<hostname\>.local - подключение к raspberry.
+
+---
+```bash
+sudo apt install nginx
+sudo nano /etc/nginx/sites-enabled/default
+```
+```
+location / {
+    proxy_pass http://127.0.0.1:5000;
+
+    proxy_set_header Host $host;
+    proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
+}
+```
--- servo_iv/web.py
+++ servo_iv/web.py
@@ -1,13 +1,24 @@
-from flask import Flask, render_template
-
+from flask import Flask, render_template, request, redirect, url_for
 
 app = Flask(__name__)
 
+messages = []
 
-@app.route('/')
-def home():
-    return render_template("index.html")
+@app.route("/", methods=["GET", "POST"])
+def index():
+    global messages
 
+    if request.method == "POST":
+        text = request.form.get("text")
+        if text:
+            messages.append(text)
+        return redirect(url_for("index"))
+
+    return render_template(
+        "index.html",
+        message=messages,
+        buttons=["Button 1", "Button 2", "Button 3"]
+    )
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=False)
+    app.run(host='0.0.0.0', port=5000, debug=True)
