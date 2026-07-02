9a3ed94b6c0873512a50135c5d143abc9ea42fd0
Ilya Slatov <slatovil@gmail.com>
2026-07-02 16:04:19 +0300

    site without function

--- servo_iv/web.py
+++ servo_iv/web.py
@@ -1,24 +1,34 @@
 from flask import Flask, render_template, request, redirect, url_for
 
-app = Flask(__name__)
+class Site:
+    def __init__(self):
+        self.app = Flask(__name__)
+        self.messages = []
+        self.buttons = ["Hello", "Hi", "Bye"]
 
-messages = []
+        self.app.add_url_rule(
+            "/",
+            endpoint="index",
+            view_func=self.index,
+            methods=["GET", "POST"]
+        )
 
-@app.route("/", methods=["GET", "POST"])
-def index():
-    global messages
+    def index(self):
+        if request.method == "POST":
+            text = request.form.get("text")
+            btn = request.form.get("button_click")
+            if text:
+                self.messages.append(text)
+            elif btn:
+                self.messages.append(btn)
+            return redirect(url_for("index"))
 
-    if request.method == "POST":
-        text = request.form.get("text")
-        if text:
-            messages.append(text)
-        return redirect(url_for("index"))
+        return render_template(
+            "index.html",
+            message=self.messages,
+            buttons=self.buttons
+        )
 
-    return render_template(
-        "index.html",
-        message=messages,
-        buttons=["Button 1", "Button 2", "Button 3"]
-    )
-
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=True)
+if __name__ == "__main__":
+    site = Site()
+    site.app.run(host="0.0.0.0", port=5000, debug=True)
